257bf68e2f6d91405d2b60d886db95304b9d570f
rafi-3 <nws04_01089114@pb.edu.bn>
2018-02-07 12:06:03 +0800

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {}
+  // loading 
+  presentLoading() {
+    this.loadingCtrl.create({
+      content: 'Redirecting...',
+      duration: 3000,
+      dismissOnPageChange: true
+    }).present();
   }
 
 }
+
