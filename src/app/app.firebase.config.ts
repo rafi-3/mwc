ef25342843135833fb39d002a872a0d6e6d6ccbc
rafi-3 <nws04_01089114@pb.edu.bn>
2018-02-13 00:06:24 +0800

    new login page(bugs not resolve)

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASED_CONFIG = var config = {
+    apiKey: "AIzaSyBhrcLCQgiZRn-ccR3vqHc7BHGshDOQ5v8",
+    authDomain: "mwc1-4858e.firebaseapp.com",
+    databaseURL: "https://mwc1-4858e.firebaseio.com",
+    projectId: "mwc1-4858e",
+    storageBucket: "mwc1-4858e.appspot.com",
+    messagingSenderId: "404753915351"
+}; 
